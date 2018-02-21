ecd91eb865e0d5843ab765a5c2421ba5ec3fcb46
mongveasna <veasnamongb@gmail.com>
2018-02-21 14:55:23 +0700

    - test user route

--- route/user.js
+++ route/user.js
@@ -2,22 +2,6 @@ module.exports = {
     prefix: "/user",
     routes: [
         {
-            before: "Auth@onAuthAccessToken",
-            handler: "user@onRead",
-            path: "/read",
-            method: "POST"
-        },
-        {
-            handler: "user@onCreate",
-            path: "/create",
-            method: "POST"
-        },
-        {
-            handler: "user@onLogin",
-            path: "/login",
-            method: "POST"
-        },
-        {
             before: ['Auth@loginRequired'],
             handler: "user@testToken",
             path: "/test",
